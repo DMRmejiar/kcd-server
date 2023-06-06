f403cf2ffd6dddac493556c7db33dd5407c5abab
Alejandro Alvarez <alejandro@micontrato.co>
2023-06-06 10:43:35 -0500

    add rekognition

---
+++ collection.js
@@ -0,0 +1,69 @@
+const {
+  CreateCollectionCommand,
+  IndexFacesCommand,
+  SearchFacesByImageCommand,
+  RekognitionClient,
+} = require("@aws-sdk/client-rekognition");
+const fs = require("fs");
+const REGION = "us-east-1";
+const collectionName = "kcd-collection";
+const rekogClient = new RekognitionClient({
+  region: REGION,
+  credentials: {
+    accessKeyId: "AKIARCGSWNIGFKKYFZON",
+    secretAccessKey: "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",
+  },
+});
+
+const createCollection = async (collectionName) => {
+  try {
+    console.log(`Creating collection: ${collectionName}`);
+    const data = await rekogClient.send(
+      new CreateCollectionCommand({ CollectionId: collectionName })
+    );
+    console.log("Collection ARN:");
+    console.log(data.CollectionArn);
+    console.log("Status Code:");
+    console.log(String(data.StatusCode));
+    console.log("Success.", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err.stack);
+  }
+};
+
+const saveImage = async (collectionName, imageName) => {
+  try {
+    const data = await rekogClient.send(
+      new IndexFacesCommand({
+        CollectionId: collectionName,
+        Image: { Bytes: fs.readFileSync(imageName) },
+        ExternalImageId: "647edfbe46bf470a1a36192e",
+      })
+    );
+    console.log("Faces indexed:");
+    console.log(JSON.stringify(data));
+  } catch (err) {
+    console.log("Error", err.stack);
+  }
+};
+
+const findByImage = async (collectionName, imageName) => {
+  try {
+    const data = await rekogClient.send(
+      new SearchFacesByImageCommand({
+        CollectionId: collectionName,
+        Image: { Bytes: fs.readFileSync(imageName) },
+      })
+    );
+    console.log("Faces indexed:");
+    console.log(data.FaceMatches[0].Face.ExternalImageId);
+    console.log(data.FaceMatches[0].Similarity);
+  } catch (err) {
+    console.log("Error", err.stack);
+  }
+};
+
+//createCollection(collectionName);
+//saveImage(collectionName, "./edi.jpeg");
+//findByImage(collectionName, "./edi2.jpeg");
--- plugins/validate.js
+++ plugins/validate.js
@@ -1,8 +1,33 @@
-'use strict';
-const fp = require('fastify-plugin');
+"use strict";
+const fp = require("fastify-plugin");
+const {
+  SearchFacesByImageCommand,
+  RekognitionClient,
+} = require("@aws-sdk/client-rekognition");
+const fs = require("fs");
+const REGION = "us-east-1";
+const collectionName = "kcd-collection";
+const rekogClient = new RekognitionClient({
+  region: REGION,
+  credentials: {
+    accessKeyId: "AKIARCGSWNIGFKKYFZON",
+    secretAccessKey: "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",
+  },
+});
 
 module.exports = fp(async function (fastify, opts) {
-	fastify.decorate('validateUser', function (image) {
-		return '647edfbe46bf470a1a36192e';
-	});
+  fastify.decorate("validateUser", async function (image) {
+    const imageBuffer = Buffer.from(image, "base64");
+    const data = await rekogClient.send(
+      new SearchFacesByImageCommand({
+        CollectionId: collectionName,
+        Image: { Bytes: imageBuffer },
+      })
+    );
+    console.log("Faces indexed:");
+    const id = data.FaceMatches[0].Face.ExternalImageId;
+    console.log(id);
+    console.log(data.FaceMatches[0].Similarity);
+    return id;
+  });
 });
